255e4a0798053543ab9b647ab38b4dd6436f05b0
Jourdan <NPStudent@S10166869-PC>
2017-07-23 20:29:54 +0800

    I created a LoginModel in the Models/ViewModels folder

---
+++ src/MovieStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Models;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required] //Ensures that values are provided
+        public string Name { get; set; }
+
+        [Required]
+        [UIHint("password")] //Text will be hashed (for confidentiality purposes)
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
